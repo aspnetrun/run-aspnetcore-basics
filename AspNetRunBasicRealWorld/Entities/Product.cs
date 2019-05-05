a3c085da9d72f64c73dfc1d954f63472c4a6ad33
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-05-05 17:00:34 +0300

    Product entity added

---
+++ AspNetRunBasicRealWorld/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetRunBasicRealWorld.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(80)]
+        public string Name { get; set; }
+
+        [Required, StringLength(255)]
+        public string Description { get; set; }
+
+        public int UnitPrice { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
