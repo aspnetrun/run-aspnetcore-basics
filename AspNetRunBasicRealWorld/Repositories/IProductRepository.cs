91dd53bb588c6d024ee70384990c73525c03103c
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-05-05 17:05:45 +0300

    IProductRepository added with repository structure

---
+++ AspNetRunBasicRealWorld/Repositories/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using AspNetRunBasicRealWorld.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AspNetRunBasicRealWorld.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductListAsync();
+        Task<Product> GetProductByIdAsync(int id);
+        Task<IEnumerable<Product>> GetProductByNameAsync(string name);
+        Task<IEnumerable<Product>> GetProductByCategoryAsync(int categoryId);
+        Task<Product> AddAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task DeleteAsync(Product product);
+        Task<IEnumerable<Category>> GetCategories();
+    }
+}
