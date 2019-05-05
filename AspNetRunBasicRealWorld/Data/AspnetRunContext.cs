e4004b9fc13df53bf597e5380604a7387f93363e
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-05-05 17:03:53 +0300

    AspnetRunContext added with EF.Core packages

---
+++ AspNetRunBasicRealWorld/Data/AspnetRunContext.cs
@@ -0,0 +1,16 @@
+﻿using AspNetRunBasicRealWorld.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetRunBasicRealWorld.Data
+{
+    public class AspnetRunContext : DbContext
+    {
+        public AspnetRunContext(DbContextOptions<AspnetRunContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
