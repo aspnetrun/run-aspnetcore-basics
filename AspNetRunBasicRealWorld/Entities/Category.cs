739b34bb3bf9e43bfc846dc8ce86094b0f9c55db
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-05-05 17:01:04 +0300

    Category entity added

---
+++ AspNetRunBasicRealWorld/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetRunBasicRealWorld.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(80)]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
