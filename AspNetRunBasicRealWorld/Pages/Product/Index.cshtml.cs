3ed3285a37ec0367d9137b4533b8e02f367f2594
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-05-06 17:17:27 +0300

    IndexModel of Product added

---
+++ AspNetRunBasicRealWorld/Pages/Product/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AspNetRunBasicRealWorld.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetRunBasicRealWorld.Pages.Product
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IProductRepository _productRepository;
+
+        public IndexModel(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+        }
+
+        public IEnumerable<Entities.Product> ProductList { get; set; } = new List<Entities.Product>();
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchTerm { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            ProductList = await _productRepository.GetProductByNameAsync(SearchTerm);
+            return Page();
+        }        
+    }
+}
