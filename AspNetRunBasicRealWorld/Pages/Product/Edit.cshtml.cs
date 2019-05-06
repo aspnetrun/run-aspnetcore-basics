09952b313cbbe36fb2d63d988bb59875bca91677
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-05-06 17:18:44 +0300

    EditModel created

---
+++ AspNetRunBasicRealWorld/Pages/Product/Edit.cshtml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading.Tasks;
+using AspNetRunBasicRealWorld.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetRunBasicRealWorld.Pages.Product
+{
+    public class EditModel : PageModel
+    {
+        private readonly IProductRepository _productRepository;
+
+        public EditModel(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+        }
+
+
+        [BindProperty]
+        public Entities.Product Product { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? productId)
+        {
+            if (productId == null)
+            {
+                return NotFound();
+            }
+
+            Product = await _productRepository.GetProductByIdAsync(productId.Value);
+            if (Product == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["CategoryId"] = new SelectList(await _productRepository.GetCategories(), "Id", "Name");
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            try
+            {
+                await _productRepository.UpdateAsync(Product);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(Product.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToPage("./Index");
+        }
+
+        private bool ProductExists(int id)
+        {
+            var product = _productRepository.GetProductByIdAsync(id);
+            return product != null;
+        }
+    }
+}
