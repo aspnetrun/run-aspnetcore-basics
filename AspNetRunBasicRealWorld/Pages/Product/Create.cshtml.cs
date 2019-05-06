1b2d1dd9d8d59765b615454f586e0a7352c9ac9b
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-05-06 17:21:52 +0300

    CreateModel added

---
+++ AspNetRunBasicRealWorld/Pages/Product/Create.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using AspNetRunBasicRealWorld.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AspNetRunBasicRealWorld.Pages.Product
+{
+    public class CreateModel : PageModel
+    {
+        private readonly IProductRepository _productRepository;
+
+        public CreateModel(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var categories = await _productRepository.GetCategories();
+            ViewData["CategoryId"] = new SelectList(categories, "Id", "Name");
+            return Page();
+        }
+
+        [BindProperty]
+        public Entities.Product Product { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            Product = await _productRepository.AddAsync(Product);
+            return RedirectToPage("./Index");
+        }
+    }
+}
