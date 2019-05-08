f6834b9e1f87d433595b075117259c176c9ff95e
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-05-08 13:57:35 +0300

    IdentityDbContext

--- AspNetRunBasicRealWorld/Data/AspnetRunContext.cs
+++ AspNetRunBasicRealWorld/Data/AspnetRunContext.cs
@@ -1,9 +1,10 @@
 ﻿using AspNetRunBasicRealWorld.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace AspNetRunBasicRealWorld.Data
 {
-    public class AspnetRunContext : DbContext
+    public class AspnetRunContext : IdentityDbContext
     {
         public AspnetRunContext(DbContextOptions<AspnetRunContext> options)
             : base(options)
