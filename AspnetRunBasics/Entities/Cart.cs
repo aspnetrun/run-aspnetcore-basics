2ad17970e1717651d3b60c0b52053bcbd7947ec5
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-02-06 22:26:06 +0300

    entities added

---
+++ AspnetRunBasics/Entities/Cart.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace AspnetRunBasics.Entities
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public List<CartItem> Items { get; set; } = new List<CartItem>();
+    }
+}
---
+++ AspnetRunBasics/Entities/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace AspnetRunBasics.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
