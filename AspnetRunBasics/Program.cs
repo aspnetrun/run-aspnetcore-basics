f1e142168bef3d03a94a5d898b4227d1495ccafa
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-02-14 15:06:11 +0300

    index page finished

--- AspnetRunBasics/Program.cs
+++ AspnetRunBasics/Program.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspnetRunBasics.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +15,9 @@ namespace AspnetRunBasics
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            SeedDatabase(host);
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +26,25 @@ namespace AspnetRunBasics
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void SeedDatabase(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+                try
+                {
+                    var aspnetRunContext = services.GetRequiredService<AspnetRunContext>();
+                    AspnetRunContextSeed.SeedAsync(aspnetRunContext, loggerFactory).Wait();
+                }
+                catch (Exception exception)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(exception, "An error occurred seeding the DB.");
+                }
+            }
+        }
     }
 }
