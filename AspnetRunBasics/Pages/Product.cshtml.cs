073213e01c9e0f7a1baa5a5d313f70d7a497e7e2
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-02-17 08:17:09 +0300

    Product Page completed

--- AspnetRunBasics/Pages/Product.cshtml.cs
+++ AspnetRunBasics/Pages/Product.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspnetRunBasics.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +10,35 @@ namespace AspnetRunBasics
 {
     public class ProductModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly IProductRepository _productRepository;
 
+        public ProductModel(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+        }
+
+        public IEnumerable<Entities.Category> CategoryList { get; set; } = new List<Entities.Category>();
+        public IEnumerable<Entities.Product> ProductList { get; set; } = new List<Entities.Product>();
+
+
+        [BindProperty(SupportsGet = true)]
+        public string SelectedCategory { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? categoryId)
+        {
+            CategoryList = await _productRepository.GetCategories();
+
+            if (categoryId.HasValue)
+            {
+                ProductList = await _productRepository.GetProductByCategoryAsync(categoryId.Value);
+                SelectedCategory = CategoryList.FirstOrDefault(c => c.Id == categoryId.Value)?.Name;
+            }
+            else
+            {
+                ProductList = await _productRepository.GetProductListAsync();
+            }
+
+            return Page();
         }
     }
 }
